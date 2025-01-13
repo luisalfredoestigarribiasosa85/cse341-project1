753e1192f7cc732abb442d8ec2afe84a644eb261
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2025-01-13 09:13:50 -0300

    Finished project1!

---
+++ controllers/contacts.js
@@ -0,0 +1,47 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+// Obtener todos los contactos
+const getAll = async (req, res) => {
+    try {
+        const result = await mongodb.getDatabase().db().collection('contacts').find();
+        const contacts = await result.toArray();
+
+        res.setHeader('Content-type', 'application/json');
+        res.status(200).json(contacts);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch contacts' });
+    }
+};
+
+
+const getSingle = async (req, res, next) => {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    try {
+        const objectId = new ObjectId(id);
+        const contact = await mongodb
+            .getDatabase()
+            .db()
+            .collection('contacts')
+            .findOne({ _id: objectId });
+
+        if (!contact) {
+            return res.status(404).json({ error: 'Contact not found' });
+        }
+
+        res.setHeader('Content-type', 'application/json');
+        res.status(200).json(contact);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch contact' });
+    }
+};
+
+module.exports = {
+    getAll,
+    getSingle
+};
