67467e04d0fa00298486569d98fe5dcc7f3ee7ae
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2025-01-18 22:38:33 -0300

    finished contacts part 2

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(bodyParser.json());
 app.use('/', require('./routes/contacts'));
 
 mongodb.initDb((err) => {
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -40,7 +40,68 @@ const getSingle = async (req, res) => {
     }
 };
 
+const createContact = async (req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    try {
+        const result = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+        if (result.acknowledged) {
+            res.status(201).send();
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create contact' });
+    }
+};
+
+const updateContact = (req, res) => {
+    const id = req.params.id;
+    const contactId = new ObjectId(id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const query = { _id: contactId };
+    const options = { upsert: true };
+    const updateDoc = {
+        $set: contact
+    };
+    try {
+        const result = mongodb.getDatabase().db().collection('contacts').updateOne(query, updateDoc, options);
+        if (result.modifiedCount > 0) {
+            res.status(201).send();
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update contact' });
+    }
+};
+
+const deleteContact = (req, res) => {
+    const id = req.params.id;
+    const contactId = new ObjectId(id);
+    const query = { _id: contactId };
+    try {
+        const result = mongodb.getDatabase().db().collection('contacts').deleteOne(query);
+        if (result.deletedCount > 0) {
+            res.status(201).send();
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete contact' });
+    }
+}
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
 };
