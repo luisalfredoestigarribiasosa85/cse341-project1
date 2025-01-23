52b17acee5fe89da5d140692a8f6a7026b143b63
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2025-01-23 11:34:03 -0300

    Finished team activity

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
+const validation = require('../middleware/validate');
 
 router.get('/contacts', contactsController.getAll);
 router.get('/contacts/:id', contactsController.getSingle);
-router.post('/contacts', contactsController.createContact);
-router.put('/contacts/:id', contactsController.updateContact);
+router.post('/contacts', validation.saveContact, contactsController.createContact);
+router.put('/contacts/:id', validation.saveContact, contactsController.updateContact);
 router.delete('/contacts/:id', contactsController.deleteContact);
 module.exports = router;
--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Description'
     },
-    host: 'localhost:3000',
+    host: 'https://cse341-project1-13qq.onrender.com',
     schemes: ['https', 'http'],
 };
 
